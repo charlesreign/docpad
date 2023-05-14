bb7592af8cd5b160275ca2d5f626ba6ee830552e
charlie <charlesgold45@gmail.com>
2023-05-14 21:44:30 +0000

    docpad-004 create CRUD endpoint and auth for users

--- lib/docpad/app.py
+++ lib/docpad/app.py
@@ -1,18 +1,31 @@
-import os
-
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from lib.docpad.routers import auth
+from lib.docpad.routers import get_user
+from fastapi.middleware.cors import CORSMiddleware
+from config.config import settings
 
+app = FastAPI(title="Docpad-Api")
 
-app = FastAPI(title="docpad-api")
+origins = [
+    settings.CLIENT_ORIGIN,
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, tags=['Auth'], prefix='/api/v1/auth')
+app.include_router(get_user.router, tags=['Users'], prefix='/api/v1/users')
 
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/docs", status_code=301)
 
-
 @app.get("/health")
 async def health():
-    return {"response": "docpad api is healthy"}
+    return {"response": "Docpad Api is healthy"}
